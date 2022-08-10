6a16bd59d1fbd88f24adb12f5de1a1b3797caafe
tootaemi <108526267+tootaemi@users.noreply.github.com>
2022-08-10 13:37:24 +0000

    Initial Commit1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  post 'lists' => 'lists#create'  
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
